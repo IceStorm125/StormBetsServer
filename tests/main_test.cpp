7397b67d8b46f916d146170e13ee354413e8bc12
nano <arsenal1251@mail.ru>
2024-03-20 15:11:46 +0300

    add: unit tests for Client

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -1,77 +1,7 @@
-#include "regexmatcher.h"
-
 #include <gtest/gtest.h>
 
-TEST(RegexMatcherTest, PositiveIntegerString)
-    {
-        EXPECT_TRUE(RegexMatcher::isStringPositiveNumber("123"));
-    }
-
-    TEST(RegexMatcherTest, NegativeIntegerString)
-    {
-        EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("-123"));
-    }
-
-    TEST(RegexMatcherTest, StringWithLeadingZeros)
-    {
-        EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("00123"));
-    }
-
-    TEST(RegexMatcherTest, EmptyString)
-    {
-        EXPECT_FALSE(RegexMatcher::isStringPositiveNumber(""));
-    }
-
-    TEST(RegexMatcherTest, StringWithLetters)
-    {
-        EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("123abc"));
-    }
-    
-    TEST(RegexMatcherTest, StringWithSpaces)
-    {
-        EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("    "));
-    }
-
-    TEST(RegexMatcherTest, StringWithDots)
-    {
-        EXPECT_FALSE(RegexMatcher::isStringPositiveNumber(".123"));
-    }
-
-    TEST(RegexMatcherTest, StringStartsWithDot)
-    {
-        EXPECT_FALSE(RegexMatcher::isStringPositiveNumber(".1234"));
-    }
-
-    TEST(RegexMatcherTest, StringStartsWithMinusAndDot)
-    {
-        EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("-.1234"));
-    }
-
-    TEST(RegexMatcherTest, StringStartsWithMinusAndDot2)
-    {
-        EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("-0.1234"));
-    }
-
-    TEST(RegexMatcherTest, StringWithDash)
-    {
-        EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("123-456"));
-    }
-
-    TEST(RegexMatcherTest, StringWithPlus)
-    {
-        EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("123+456"));
-    }
-
-    TEST(RegexMatcherTest, StringWithPlusAndLeadingSpace)
-    {
-        EXPECT_FALSE(RegexMatcher::isStringPositiveNumber(" 123+456"));
-    }
-
-    TEST(RegexMatcherTest, StringWithLeadingLetter)
-    {
-        EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("a123"));
-    }
-
+#include "regex_test.hpp"
+#include "client_test.hpp"
 
 int main(int argc, char *argv[])
 {
---
+++ tests/regex_test.hpp
@@ -0,0 +1,74 @@
+#include "regexmatcher.h"
+
+#include <gtest/gtest.h>
+
+TEST(RegexMatcherTest, PositiveIntegerString)
+{
+    EXPECT_TRUE(RegexMatcher::isStringPositiveNumber("123"));
+}
+
+TEST(RegexMatcherTest, NegativeIntegerString)
+{
+    EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("-123"));
+}
+
+TEST(RegexMatcherTest, StringWithLeadingZeros)
+{
+    EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("00123"));
+}
+
+TEST(RegexMatcherTest, EmptyString)
+{
+    EXPECT_FALSE(RegexMatcher::isStringPositiveNumber(""));
+}
+
+TEST(RegexMatcherTest, StringWithLetters)
+{
+    EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("123abc"));
+}
+
+TEST(RegexMatcherTest, StringWithSpaces)
+{
+    EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("    "));
+}
+
+TEST(RegexMatcherTest, StringWithDots)
+{
+    EXPECT_FALSE(RegexMatcher::isStringPositiveNumber(".123"));
+}
+
+TEST(RegexMatcherTest, StringStartsWithDot)
+{
+    EXPECT_FALSE(RegexMatcher::isStringPositiveNumber(".1234"));
+}
+
+TEST(RegexMatcherTest, StringStartsWithMinusAndDot)
+{
+    EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("-.1234"));
+}
+
+TEST(RegexMatcherTest, StringStartsWithMinusAndDot2)
+{
+    EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("-0.1234"));
+}
+
+TEST(RegexMatcherTest, StringWithDash)
+{
+    EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("123-456"));
+}
+
+TEST(RegexMatcherTest, StringWithPlus)
+{
+    EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("123+456"));
+}
+
+TEST(RegexMatcherTest, StringWithPlusAndLeadingSpace)
+{
+    EXPECT_FALSE(RegexMatcher::isStringPositiveNumber(" 123+456"));
+}
+
+TEST(RegexMatcherTest, StringWithLeadingLetter)
+{
+    EXPECT_FALSE(RegexMatcher::isStringPositiveNumber("a123"));
+}
+
