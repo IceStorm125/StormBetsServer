01ffc4750237cbbfaa0dcb9675ceb3d04f7bd088
nano <arsenal1251@mail.ru>
2024-05-08 16:12:22 +0300

    modify: add conan commands

--- install_build_dep.sh
+++ install_build_dep.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-apt update -y && apt upgrade -y
-
 apt install -y g++ make git binutils cmake libssl-dev libboost-system-dev zlib1g-dev build-essential curl libcurl4-openssl-dev
 apt install -y qtcreator qtbase5-dev qt5-qmake clang
 apt install -y mysql-server libqt5sql5-mysql 
 snap install mysql-workbench-community
+
+conan profile detect --force
+conan install . --build=missing
+
+cd build
+cmake ..
+make -j4
