59707858fdc6671e36f11711cc332a583ad8c37d
nano <arsenal1251@mail.ru>
2024-05-08 16:13:52 +0300

    modify: add new emojis

--- include/messagestosend.h
+++ include/messagestosend.h
@@ -10,6 +10,10 @@ const std::string FOLDED_HANDS = "\xF0\x9F\x99\x8F";
 const std::string CHECK_MARK = "\xE2\x9C\x85";
 const std::string CROSS_MARK = "\xE2\x9D\x8C";
 const std::string BACK_MARK = "\xF0\x9F\x94\x99";
+const std::string STAT = "\xF0\x9F\x93\x88";
+const std::string TROPHY = "\xF0\x9F\x8F\x86";
+const std::string MONEY_WITH_WINGS = "\xF0\x9F\x92\xB8";
+const std::string DOLLAR_MONEY = "\xF0\x9F\x92\xB5";
 }
 
 namespace Messages {
@@ -18,6 +22,7 @@ const std::string CURRENT_BETS = Emojis::FOLDED_HANDS + " My current bets";
 const std::string COINS = Emojis::MONEY + " My coins";
 const std::string PLAYED_BETS = Emojis::CHECK_MARK+ " Played bets";
 const std::string DELETE_BET = Emojis::BACK_MARK + " Return bet";
+const std::string STATS = Emojis::STAT + " My statistics";
 const std::string LOGIN = "You are not logged in, please, use /start command";
 const std::string LOADING = "Loading matches...";
 const std::string CHOOSE_OPTION = "Please, choose option";
