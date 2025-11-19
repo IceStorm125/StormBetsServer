c7e5d3a6fb170b2dcd09d3c70b7c074861541885
vboxuser <arsenal1251@mail.ru>
2025-11-19 20:34:46 +0000

    add: check if match is closer then week

--- src/dtos/matchdto.cpp
+++ src/dtos/matchdto.cpp
@@ -32,7 +32,7 @@ std::vector<Match> MatchDTO::getAllMatches()
         double koef_W2 = record.value(7).toDouble();
         QDateTime time = record.value(8).toDateTime();
 
-        if(time > QDateTime::currentDateTime().addSecs(15*60))
+        if(time > QDateTime::currentDateTime().addSecs(15*60) && time < QDateTime::currentDateTime().addDays(7))
         {
             Match match(id, api_id, title, team1, team2, koef_W1, koef_X, koef_W2, time);
             matches.push_back(match);
