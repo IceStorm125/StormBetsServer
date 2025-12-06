70dd584796c0ba639c217cccd7bf766b06216922
IceStorm125 <55408503+IceStorm125@users.noreply.github.com>
2025-12-06 21:53:23 +0300

    modify: update if session expired check time

--- src/main.cpp
+++ src/main.cpp
@@ -425,7 +425,7 @@ int main(int argc, char *argv[]) {
     auto userSessionTread = std::thread([&userSessions, &currentProceses, &sessionMutex] {
         while (true) {
             cleanupExpiredSessions(userSessions, currentProceses, sessionMutex);
-            std::this_thread::sleep_for(std::chrono::seconds(1));
+            std::this_thread::sleep_for(std::chrono::minutes(1));
         }
     });
 
